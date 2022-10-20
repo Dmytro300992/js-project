b80c7387a9eb3b785e1baf038c627c2c44726fd2
dmytro <kirichenkod092@gmail.com>
2022-10-20 20:09:14 +0300

    Escape

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
-let num = 0.1 + 0.7;
-console.log(num);
+const sum = (2 + 2) * 6;
+console.log(sum);
 
-let num1 = NaN;
 
-let a = 1 + 2;
-let b = 2 - 1;
-let c = 2 * 2;
-let d = 4 / 2;
-
-let notAllowed = 5 / 0;
-console.log(notAllowed);
-let divTo0 = 4 / Infinity
-console.log(divTo0)
-
-let newLine = 'Hello' + ' you';
-console.log(newLine);
-
-newLine = 5 + 'Hello';
-console.log(newLine);
-
-newLine = 11 - '1';
-console.log(newLine);
+const hello = 'Маша сказала \n"привет\"';
+console.log(hello);
