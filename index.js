9e3f9a30f350531d814e2a8cf06c7835332d7ed8
dmytro <kirichenkod092@gmail.com>
2022-10-20 19:08:21 +0300

    Concatenation

---
+++ index.js
@@ -0,0 +1,23 @@
+let num = 0.1 + 0.7;
+console.log(num);
+
+let num1 = NaN;
+
+let a = 1 + 2;
+let b = 2 - 1;
+let c = 2 * 2;
+let d = 4 / 2;
+
+let notAllowed = 5 / 0;
+console.log(notAllowed);
+let divTo0 = 4 / Infinity
+console.log(divTo0)
+
+let newLine = 'Hello' + ' you';
+console.log(newLine);
+
+newLine = 5 + 'Hello';
+console.log(newLine);
+
+newLine = 11 - '1';
+console.log(newLine);
