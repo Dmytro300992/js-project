76ade9b372a6b122bf9b95072d6226816eb54e31
dmytro <kirichenkod092@gmail.com>
2022-10-27 19:07:27 +0300

    Object function

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-let obj = {
-        color: 'red',
-        fontSize: 2
-    };
+const cat = {
+    age: 3
+}
 
-    let bulb = {
-        producer: 'Philips',
-        lightness: 200,
-        power: '200w',
-        cap: 'small',
-        on: function() {
-            return 'The light on!'
-        }
-    }
+console.log(cat.age);
 
-    console.log(bulb.cap);
-    console.log(bulb.on());
+cat.age = 5;
+console.log(cat.age); // Літеральне створення
+
+const cat2 = Object{};
+const cat3 = new Object{};
