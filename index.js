84cee4f77150e54ae8d3833b03d4032f59c20ffc
dmytro <kirichenkod092@gmail.com>
2022-11-01 13:37:32 +0200

    Home task 28.10.22

--- index.js
+++ index.js
@@ -1,16 +1,66 @@
-const user = {
-    name: 'John',
-    lastName: 'Doe',
-    age: 20,
-    'favorite color': 'red',
-    movie: 'Blue moon',
-    avatar: 'http://.....'
+/*28.10.2022
+Задача 3*/
+
+const obj = {
+    Mon: 'driver lesson',
+    Tue: 'dantist visit',
+    Wed: 'go to party',
+    Thu: 'chill',
+    Fri: 'hard work',
+    Sat: 'go to park'
 }
 
-// for ... in
+let userValue = prompt('Type your week day your plan: \n Monday = Mon, \n Tusdey = Tue ... e.t.c');
+alert(obj[userValue] ? obj[userValue] : 'Nothing planned');
 
-// НЕ ЦЕЙ for(;;)
-
-for (let key in user) {
-    console.log(`${key} ---- ${user[key]}`); // key ---- value
+/*Задача 4
+Написати функцію, яка повертає загальну суму, витрачену на зарплатню всіх відділів
+const departmentSalary = {
+    HR: 20000,
+    Development: 30000,
+    Drive: 200,
+    Clining: null,
+    Marketing: 'not salary'
 }
+(for .. in)*/
+
+const departmentSalary = {
+    HR: 20000,
+    Development: 30000,
+    Drive: 200,
+    Clining: null,
+    Marketing: 'not salary'
+}
+
+function sumSalary(salaryObj) {
+    let result = 0;
+
+    for(let key in salaryObj) {
+        if(typeof salaryObj[key] === 'number') {
+            result = result + salaryObj[key];
+        }
+    }
+
+    return result;
+}
+
+console.log(sumSalary(departmentSalary));
+
+/*
+Задача 5
+Написати функцію, яка приймає об'єкт і виводить всі значення всіх властивостей, оформлені у *....*
+const obj = {
+    key1: value2
+}
+// "key1": *value2*
+(for ... in)
+*/
+
+
+function stylingConsole(obj) {
+    for(let key in obj) {
+        console.log(`"${key}": *${obj[key]}*`);
+    }
+}
+
+stylingConsole(obj);
