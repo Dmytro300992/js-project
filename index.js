e870577a6f1ab3f81897d8a42ada7b2a15a55e84
dmytro <kirichenkod092@gmail.com>
2022-11-01 20:28:10 +0200

    Practic 01.11.22

--- index.js
+++ index.js
@@ -1,37 +1,52 @@
-//slice
-/*const array = [1, 2, 3, 4,];
+/*Задача 1*/
+/*const arr = [1, 2, 3, 4, 5];
+arr.splice(3, 4);*/
 
-console.log(array.slice(3));
-console.log(array.slice(2));
+/*Задача 3*/
+/*const arr = [1,2,3,4,5];
 
-const users = [
-    {
-        name: 'John',
-        lastName: 'Doe'
-    },
-    {
-        name: 'Jane',
-        lastName: 'Doe'
-    },
-]*/
+console.log(arr.splice(1, 0, 'w' ));
+console.log(arr.splice(2, 0, 're-td' ));
+console.log(arr.splice(7, 0, 'vv' ));
+console.log(arr.splice(6, 0, 'aa' ));
+console.log(arr)*/
 
-//mutattion
-/*const user = {
-    name: 'Humty Dum'
+
+/*arr.splice(1,0,'w','re-td');
+arr.splice(6,0,'vv');
+arr.splice(8,0,'aa');
+console.log(arr);*/
+
+/*Задача 4*/
+
+/*const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [1, 2, 2, 3, 4];
+
+function number(arr5, arr10) {
+    if(arr1 === true) {
+        return true;
+    }
+    if(arr2 == false) {
+        return false;
+    }
 }
 
-user.isBroken = false;
+console.log(number());*/
 
-const egg = 'Humty Dum';
-egg.isBroken = false;
-console.log(egg);
-console.log(egg.isBroken);*/
+/*function saySomething(howToSay, whatToSay) {
+    howToSay(whatToSay);
+}
 
-//splice
-const arr = [1, 2, 3, 4, 5];
+saySomething(alert, 'Hello');
+console.log(saySomething);*/
 
-//console.log(arr.splice(2,1));
-//console.log(arr);
+//map metod
+function square(a) {
+    return a*a;
+}
 
-console.log(arr.splice(1, 0, 'hello'));
-console.log(arr);
+const array = [1,2,3,4,5];
+
+const newArray = array.map((item) => {
+    return item ** 2;
+})
