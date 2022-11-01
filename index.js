0e521705705e44a2bdb72448d947ba3a8c9a2dd3
dmytro <kirichenkod092@gmail.com>
2022-11-01 13:47:59 +0200

    Array methods

--- index.js
+++ index.js
@@ -1,71 +1,36 @@
-let userTel1 = '3805465465465';
-let userTel2 = '3805165161565';
-let userTel3 = '4654351321232';
+//concat
+const arr = [1, 2, 3];
+const arr2 = [5, 8, 0];
+const arr3 = arr.concat(arr2); // arr3 = arr1 + arr2
 
-const userTels = {
-    0: '3805465465465',
-    1: '3805165161565',
-    2: '4654351321232'
-}
+// fill
+const arrFive = new Array(5);
+arrFive.fill(1);
 
-// Array
+// includes
+const arr6 = [2, 4, 1, 3, 6, 3, 7];
+console.log(arr6.includes(6));
+// indexOf
+console.log(arr6.indexOf(3));
 
-const arr = [2, 4, 6, 7];
-const arr2 = new Array(2, 4, 6, 7);
+// join
+const newArr = ['Hello', 'world', 4, 2, 'hi'];
+console.log(newArr.join(' ')); // default separator - ','
 
-///////
+// push
+const oldArray = [2, 3, 4, 5];
+oldArray.push(6);
 
+// pop
+const anotherArray = [5, 6, 7, 8];
+anotherArray.pop();
 
-// вивести значення всіх елементів масиву array на єкран
-const array = [7, 3, 5, 2];
-array[0];
-array[1];
-array[2];
-array[3];
+// shift
+console.log(anotherArray.shift());
 
-for (let i = 0; i < array.length; i++) {
-    console.log(
-        array[i]
-    );
-}
+// unshift
+anotherArray.unshift(3, 6, 55);
 
-/*
-Задача 1
-Дан масив [3, 6, 8, 2, 3, 5, 1].
-Напишіть функцію, яка приймає масив в якості аргументу і повертає суму всіх елементів масиву.
-Наприклад: [1, 2, 3] // sum: 1 + 2 + 3 = 6
-*/
-const arr1 = [3, 6, 8, 2, 3, 5, 1];
-
-function sumArray(array) {
-    let sum = 0;
-
-    for(let i = 0; i < array.length ; i++) {
-        if(typeof array[i] === 'number') {
-            sum = sum + array[i];
-        }
-    }
-
-    return sum;
-}
-
-console.log(sumArray(arr1));
-
-/*
-Задача 3
-Написати функцію, яка знаходить середнє арифметичне всіх елементів масиву.
-[3, 6, 8, 2, 3, 5, 1] // 4
-*/
-
-function average(array) {
-    let sum = 0;
-    for(let i = 0; i < array.length; i++) {
-        if(typeof array[i] === 'number') {
-            sum += array[i]
-        }
-    }
-    return sum / array.length;
-}
-
-
-console.log(average(arr1));
+// reverse
+const arr10 = [1, 2, 3, 4, 5, 6];
+arr10.reverse();
