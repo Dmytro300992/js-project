0cbf0f4c99b7c8a1f3cc821d1ff73068cc568d49
dmytro <kirichenkod092@gmail.com>
2022-11-03 20:48:59 +0200

    LadderProto

--- index.js
+++ index.js
@@ -1,44 +1,61 @@
-/*MyAraay*/
-/*function MyArray() {
+/*function MyArray() { // функція-конструктор масивів
     this.length = 0;
+}
 
-    this.push = function(value) {
-        // push - добавляет єлемент, которій нам передают в конец массива
-        // изначально, длина массива (this.length = 0)
-        // куда добавлять? = что нам нужно добавить?
-        // this[this.length] = value;
-        // this.length++;
-
-        this[this.length] = value; // this[key] = value
-        this.length++;
+function myProtoArray() {
+    this.push = function() {
+        for(let i = 0; i < arguments.length; i++) {
+            this[this.length] = arguments[i];
+            this.length++;
+        }
         return this.length;
-    }
-
+    },
     this.pop = function() {
-        if(this.length > 0 ) {
+        if(this.length > 0) {
             let lastItem = this[this.length - 1];
             delete this[this.length - 1];
             this.length--;
             return lastItem;
-        } 
+        }
     }
 }
+
+MyArray.prototype = new myProtoArray;
+
 
 const arr = new MyArray();*/
 
-const cat = {
-    name: 'Barsik',
-    color: 'red',
-    age: 1
-};
+/*Задача*/
+/*
+LADDER
 
-const catMethods = {
-    run: function() {
-        console.log('Cat is running');
-    },
-    meow: function() {
-        console.log('Meow');
+Задача: створити функцію-конструктор для сходів
+Об'єкт має властивість
+currentStair // сходинка, на якій ми зараз знаходимось. Початково = 0
+
+Має методи:
+up() - піднімає на сходинку вище
+down() - опускає на сходинку нижче
+showStair() - показує, на якій сходинці ми зараз знаходимось
+*/
+
+function Ladder() {
+    this.currentStair = 0;
+}
+
+function LadderProto() {
+    this.up = function() {
+        this.currentStair++;
+        return currentStair;
+    }
+    this.down = function() {
+        this.currentStair--;
+        return currentStair;
+        }
+
+    this.shownStair = function() {
+        return this.currentStair;
     }
 }
 
-cat.__proto__ = catMethods;
+Ladder.prototype = new LadderProto;
