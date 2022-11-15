5666231cebf6ded1ac5b32f9292eccc17f888ebf
dmytro <kirichenkod092@gmail.com>
2022-11-15 17:38:28 +0200

    Symbol-Unshift

--- index.js
+++ index.js
@@ -1,107 +1,82 @@
 /*
-Практика на вихідні 11.11.2022
+Переделать под class
 
-Є ферма.
-На фермі в нас є свійські тварини (корови, кози).
-Є домашні тварини (кошки і собаки).
-
-В кожної тварини є діти. В кожного з дітей може бути певна кількість дітей.
-
-Реалізувати класи всіх тварин і додати тваринам певну кількість дітей 
-(кількість дітей вказуєте у конструкторі під час стоврення єкземпляру об'єкту).
-
-Просунутий рівень (задача *****):
-Реалізувати функцію, яка підрахує кількість тварин на фермі.
-
-Друзі, це задача досить важка.
-Тому, якщо у вас щось не вийде - будь-ласка, не засмучуйтеся.
-Ми обов'язково вирішимо цю задачу разом.
-Все буде добре =)
-Важкий матеріал по ООП ми пройшли. Всіх вітаю)
-Наступний тиждень буде легший)
-*/
-
-/*class Farm {
-	constructor(cows) {
-		this.cows = cows;
-	}
-
-	Sell() {
-		if (this.canSell()) {
-			this.cows = this.cows - 1;
-		}
-	}
-	
-	canSell(cows) {
-		if (cows > 0) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+function MyArray() {
+    this.length = 0;
 }
 
-var farm = new Farm(10);
-farm.Sell(10, 20);*/
-
-class Animal {
-    constructor() {
-        this.childs = null;
-    }
-}
-
-class Cat extends Animal {
-    constructor() {
-        super();
-    }
-}
-
-class Dog extends Animal {
-    constructor() {
-        super();
-    }
-}
-
-class Cow extends Animal {
-    constructor() {
-        super();
-    }
-}
-
-class Goat extends Animal {
-    constructor() {
-        super();
-    }
-}
-
-/**
- * Функція створення ферми
- * @param {Animal (or Cat, Dog, Cow, Goat)} Type 
- * @param {number} quantity 
- */
-
-function createFamily(Type, quantity) {
-    const animals = new Array(quantity).fill(new Type());
-    animals.forEach(animal => {
-        const childs = new Array(quantity).fill(new Type());
-        animal.childs = childs;
-    })
-    return animals;
-}
-
-/**
- * 
- * @param {Array of Animals} farm 
- */
-
-function countFarm(farm) {
-    return farm.reduce((accum, item) => {
-        if(item.childs) {
-            return accum + 1 + item.childs.length;
+function myProtoArray() {
+    this.push = function() {
+        for(let i = 0; i < arguments.length; i++) {
+            this[this.length] = arguments[i];
+            this.length++;
         }
-    }, 0)
+        return this.length;
+    },
+    this.pop = function() {
+        if(this.length > 0) {
+            let lastItem = this[this.length - 1];
+            delete this[this.length - 1];
+            this.length--;
+            return lastItem;
+        }
+    }
 }
 
-// const farm = [...createFamily(Cat, 3), ...createFamily(Dog, 2), ...createFamily(Cow, 5), ...createFamily(Goat, 1)];
+MyArray.prototype = new myProtoArray;
 
-const farm = [...createFamily(Cat, 1), ...createFamily(Dog, 1)];
+
+const arr = new MyArray();*/
+
+
+
+/*class MyArray {
+    constructor() {
+        this.length = 0;
+    }
+    
+    push() {
+        for(let i = 0; i < arguments.length; i++) {
+            this[this.length] = arguments[i];
+            this.length++;
+    }
+    return this.length;
+}
+
+pop() {
+    if(this.length > 0) {
+        let lastItem = this[this.length - 1];
+        delete this[this.length - 1];
+        this.length--;
+    }
+    return lastItem;
+}
+
+forEach(callback) {
+    for(let i = 0; i < this.length; i++) {
+        callback(this[i], i, this);
+    }
+}
+
+map(callback) {
+    const newArr = new MyArray();
+    for(let i = 0; i < this.length; i++) {
+        newArr.push(callback(this[i]))
+    }
+    return newArr;*/
+    
+
+    const b1 = true;
+    const b2 = new Boolean(1);
+
+    const str = 'hello';
+    const str2 =  new String('hello');
+
+    const MySymbol = Symbol();
+   // const symb2 = Symbol('My second symbol');
+
+    const obj = {
+        test: 1,
+        [MySymbol]: 123,
+        ab: '2123'
+    }
